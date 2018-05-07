da8b01765bcfff39b982d1817cf0fbdde0ddaaff
julio-hernandez <tenek75@gmail.com>
2018-05-07 10:24:35 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,7 +10,7 @@ class profile::ssh_server{
     ensure =>present,
     user =>'root',
     type =>'ssh-rsa',
-    key =>'AAAAB3NzaC1yc2EAAAADAQABAAABAQDq+krePAhcTuNxFwooSRlZcQ6idH3ljuaub4aLniIsFE7Hgqz5lUJAXeMQOy+6y3TbKnAglN8EBCPzPWQwYih+xR4Cl39ZtdCIRb1X1z52oTjyyZoiEhU7NwWweVihuHLwVHIvL0XK6NdNgkFfA7fCA6kld/ciE48/Hu7lH9JLLH7p76VMaav4cpn3+5p3vIcNewlOPPEuGCVZvETrdbvWBGMTcVQBDQWjDTjliFIkKc1MNbG+hSQcQ7kZ7v4uvpmI/EtIo7tMugGsmsmhvqQWeL9sShDg0H4HCYBL2e7FVMVRRbTR1TO10Wyq6Srw/M9+Eby0pInNiXZk5u6KXMKB root@master.puppet.vm',
+    key =>'AAAAB3NzaC1yc2EAAAADAQABAAABAQDq+krePAhcTuNxFwooSRlZcQ6idH3ljuaub4aLniIsFE7Hgqz5lUJAXeMQOy+6y3TbKnAglN8EBCPzPWQwYih+xR4Cl39ZtdCIRb1X1z52oTjyyZoiEhU7NwWweVihuHLwVHIvL0XK6NdNgkFfA7fCA6kld/ciE48/Hu7lH9JLLH7p76VMaav4cpn3+5p3vIcNewlOPPEuGCVZvETrdbvWBGMTcVQBDQWjDTjliFIkKc1MNbG+hSQcQ7kZ7v4uvpmI/EtIo7tMugGsmsmhvqQWeL9sShDg0H4HCYBL2e7FVMVRRbTR1TO10Wyq6Srw/M9+Eby0pInNiXZk5u6KXMKB',
   }
 
 }
